79e89055d03df9afc07c4dd9a694ff1447fde228
Ryan Jorge Abreu de Carvalho <ryan.abreu@dcx.ufpb.br>
2025-05-11 16:16:32 -0300

    change default logo to match custom one

--- keyboards/sofle/keymaps/default/config.h
+++ keyboards/sofle/keymaps/default/config.h
@@ -7,6 +7,7 @@
 #define TRI_LAYER_LOWER_LAYER 2
 #define TRI_LAYER_UPPER_LAYER 3
 #define TRI_LAYER_ADJUST_LAYER 4
+// #define OLED_FONT_H "keyboards/sofle/keymaps/default/sofle_font.c"
 // #define ENCODERS_PAD_A { F4 }
 // #define ENCODERS_PAD_B { F5 }
 // #define ENCODERS_PAD_A_RIGHT { F4 }
--- keyboards/sofle/keymaps/default/rules.mk
+++ keyboards/sofle/keymaps/default/rules.mk
@@ -3,3 +3,6 @@ ENCODER_ENABLE = yes
 ENCODER_MAP_ENABLE = yes
 
 CAPS_WORD_ENABLE = yes
+
+OLED_ENABLE = yes
+# EXTRAFLAGS += -DOLED_FONT_H=\"keymaps/default/sofle_font.c\"
