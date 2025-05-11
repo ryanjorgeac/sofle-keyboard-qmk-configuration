e2f54d0c8456d12868b2b7170281f135f1f1ab52
Ryan Jorge Abreu de Carvalho <ryan.abreu@dcx.ufpb.br>
2025-05-11 18:16:28 -0300

    fix mapped keys for ABNT2 use

--- keyboards/sofle/keymaps/default/keymap.c
+++ keyboards/sofle/keymaps/default/keymap.c
@@ -42,8 +42,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT(
   QK_GESC,   KC_1,   KC_2,    KC_3,    KC_4,    KC_5,                     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_BSPC,
   KC_TAB,   KC_Q,   KC_W,    KC_E,    KC_R,    KC_T,                     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,  KC_DEL,
-  CW_TOGG,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN,  SC_RSPC,
-  SC_LSPO,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     KC_MPLY,KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_INS,
+  KC_CAPS,   KC_A,   KC_S,    KC_D,    KC_F,    KC_G,                     KC_H,    KC_J,    KC_K,    KC_L, BR_SCLN,  SC_RSPC,
+  SC_LSPO,  KC_Z,   KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,     KC_MPLY,KC_N,    KC_M, KC_COMM,  KC_DOT, BR_SLSH,  KC_INS,
                  KC_LGUI,KC_LALT,KC_LCTL, MO(_SHORTCUTS), KC_SPC,      KC_ENT, MO(_SYMBOLS), KC_HOME, KC_END, KC_QUOT
 ),
 /* SYMBOLS  
@@ -52,7 +52,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |  \/   |   \/  |  \/  |  \/  |  \/  |  \/ |                    |   6  |   7  |   8  |   9  |   0  | F12  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | \/  |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ^  |   &  |   Up  |   (  |   )  | RS/) |
+ * | \/  |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ^  |   &  |   Up  |   (  |  ~  | RS/) |
  * |------+------+------+------+------+------|  MUTE |    |       |------+-----+------+------+------+------|
  * | \/  |  =   |  -   |  Ç   |   {[ |  ]}  |-------|    |-------|   ´  | Left |  Down | Right | \ | Print |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
@@ -63,8 +63,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_SYMBOLS] = LAYOUT(
   KC_TRNS,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
   KC_TAB,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,  KC_F12,
-  KC_TRNS, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                       KC_CIRC, KC_AMPR, KC_UP, KC_LPRN, KC_RPRN, SC_RSPC,
-  KC_TRNS,  KC_EQL, KC_MINS, BR_CCED, KC_LBRC, KC_RBRC, KC_TRNS,       KC_TRNS, BR_ACUT, KC_LEFT, KC_DOWN, KC_RIGHT, KC_NUBS, KC_PSCR,
+  KC_TRNS, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                       BR_CIRC, KC_AMPR, KC_UP, KC_LPRN, BR_TILD, SC_RSPC,
+  KC_TRNS,  KC_EQL, KC_MINS, BR_CCED, BR_LBRC, BR_RBRC, KC_TRNS,       KC_TRNS, BR_ACUT, KC_LEFT, KC_DOWN, KC_RIGHT, KC_NUBS, KC_PSCR,
                        KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
 ),
 /* SHORTCUTS
